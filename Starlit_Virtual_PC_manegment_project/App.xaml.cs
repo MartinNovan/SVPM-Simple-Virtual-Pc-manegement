06139b3f72b56f17ca1bd39b1419c43431e055eb
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2024-10-17 12:25:29 +0200

    Vytvoření projektu pro správu virtuálních počítačů

---
+++ Starlit_Virtual_PC_manegment_project/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Starlit_Virtual_PC_manegment_project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Starlit_Virtual_PC_manegment_project/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Starlit_Virtual_PC_manegment_project
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
